679870cc3bd2b9ed5f0aad1a50dee5d6e4ef7a7d
Aleksandr Ahasnyk <alexandr.chasnyk@yandex.com>
2021-08-25 23:10:11 +0300

    added example

--- Examples/dev_init.py
+++ Examples/dev_init.py
@@ -1,3 +1,7 @@
 import sys
 sys.path.append('../')
-import src.PANASONIC_VP_8122A as rf
+import src.PANASONIC_VP_8122A as panasonic
+
+rf = panasonic.com_interface()
+rf.init()
+rf.init_measurement()
